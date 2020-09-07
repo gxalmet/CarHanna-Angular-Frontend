4a97fc7af17cc9754beb1ab33903cdd1348b1d13
gxalmet <gxalmet@gmail.com>
2020-09-07 17:52:14 +0200

    commit

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,10 @@
 export var Global = {
-    URLuser: 'http://localhost:3700/api/user',
-    URLproject: 'http://localhost:3700/api/projects',
-    URLteam: 'http://localhost:3700/api/team',
+    // URLuser: 'http://localhost:3700/api/user',
+    // URLproject: 'http://localhost:3700/api/projects',
+    // URLteam: 'http://localhost:3700/api/team',
+
+    URLuser: 'http://carhanna-api.herokuapp.com/api/user',
+    URLproject: 'http://carhanna-api.herokuapp.com/api/projects',
+    URLteam: 'http://carhanna-api.herokuapp.com/api/team',
+    
 }
